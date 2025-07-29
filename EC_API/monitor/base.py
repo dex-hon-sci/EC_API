a7e901538f6d84c0c363046229275583c32540e1
Dexter <dex-hon-sci@outlook.com>
2025-07-29 13:54:30 +0800

    Add monitor module

---
+++ EC_API/monitor/base.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 23 16:32:53 2025
+
+@author: dexter
+"""
+
+class Monitor():
+    # An Object incharge of 
+    def __init__():
+        pass
+    
+    
+# MonitorActiveOrder/ MonitorActivePosition
+
+class MonitorActiveOrder():
+    def __init__():
+        return
+    
+    def request():
+        return
+
+# MonitorRealTimedata
+
+class MonitorRealTimedata():
+    def __init__():
+        return 
+    
+    def request_real_time():
+        return 
+    
+    def reset_tracker():
+        return
