573ca46ca11ea210255db662a8727a30f1a9aeca
Dexter <dex-hon-sci@outlook.com>
2025-07-29 13:53:29 +0800

    Add version file

---
+++ EC_API/__version__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 23 15:54:32 2025
+
+@author: dexter
+"""
+# %% VERSIONS
+# Default/Latest/Current version
+__version__ = '0.0.1'
+
