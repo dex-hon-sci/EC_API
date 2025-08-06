91db89bfc5c14935b68c044741d64d7188f89793
Dexter <dex-hon-sci@outlook.com>
2025-08-06 21:40:28 +0800

    Add Get contractmetadata method

--- EC_API/connect/hearback.py
+++ EC_API/connect/hearback.py
@@ -10,10 +10,11 @@ from google.protobuf import json_format
 from EC_API.ext.WebAPI.webapi_2_pb2 import ClientMsg, ServerMsg
 from EC_API.msg_validation.CQG_valid_msg_check import CQGValidMsgCheck
 
-def hearback(client, 
-             client_msg: ClientMsg, 
+def hearback(client,  
+             client_msg: ClientMsg = None,
              hearback_time: float = 30) -> ServerMsg | None:
-    # A decorator function in charge of hearing back a message after sending it
+    # A decorator function in charge of hearing back a message 
+    # immediately after sending it
     start_time = time.time()
     
     while True:
@@ -29,6 +30,17 @@ def hearback(client,
         if duration > hearback_time:
             break
         
+def get_contract_metadata(client, client_msg: ClientMsg):
+    # Check if client_msg is an information_request
+    
+    while True:
+        server_msg = client.receive_server_message()
+        check = CQGValidMsgCheck(client_msg, server_msg)
+        check.check_all()
+        return_msg =  check.return_msg() 
+        if return_msg is not None:
+            contract_metadata = server_msg.information_reports[0].symbol_resolution_report.contract_metadata
+    return contract_metadata
 
 def render_json(server_msg: ServerMsg) -> str:
     json_string = json_format.MessageToJson(server_msg, indent=2)
