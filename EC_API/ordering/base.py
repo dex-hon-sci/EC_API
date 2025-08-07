b61854e89ad759a95e37b2123768dc151c36068b
Dexter <dex-hon-sci@outlook.com>
2025-08-07 12:02:05 +0800

    Add Protocol class for LiveOrder

--- EC_API/ordering/base.py
+++ EC_API/ordering/base.py
@@ -5,34 +5,110 @@ Created on Tue Jul 29 13:19:48 2025
 
 @author: dexter
 """
-from dataclasses import dataclass, field
 from collections import Protocol
 
-from WebAPI.webapi_2_pb2 import ClientMsg, ServerMsg
-from WebAPI.trade_routing_2_pb2 import TradeSubscription as TS
-from WebAPI.order_2_pb2 import Order as Ord #Side, OrderType, Duration
-
 from EC_API.ordering.enums import *
-from EC_API.connect import ConnectCQG
+from EC_API.connect import Connect
 from EC_API.msg_validation.base import CQGValidMsgCheck
-import datetime
-from datetime import timezone
-
-
 
 class LiveOrder(Protocol):
     def __init__(self, 
-                 connect: ConnectCQG):
+                 connect: Connect,
+                 symbol_name: str,
+                 request_id: int,
+                 account_id:int):
         self._connect = connect
-        
-    def send():
-        return 
-    
+        self._symbol_name = symbol_name
+        self.request_id = request_id
+        self.account_id = account_id
 
-if __name__ == "__main__":
-    ## Usage example
-    # logon
-    # Resolve symbol (input-> symbol)
-    # Trade Subscrition (get the ORDER_ID )
-    # Send order
-    pass
+    def new_order_request(self):
+        """
+        New order request. Return a tracking ID for reference and modifications.
+
+        """
+        pass
+    
+    def modify_order_request(self, ID):
+        """
+        Modify order request. Input ID and new values in allowed fields for 
+        existing unresolved orders.
+
+        """
+        pass
+    
+    def cancel_order_request(self, ID):
+        """
+        Cancel order request. Input ID and cancel existing unresolved orders.
+        """
+        pass
+    
+    def activate_order_request(self, ID):
+        """
+        Activate order request. Input ID and activate existing suspended orders.
+        """
+        pass
+    
+    def cancelall_order_request(self):
+        """
+        Cancel all unresolved order requests based on the account_id.
+        """
+        pass
+    
+    def liquidateall_order_request(self):
+        """
+        Liquidate all open positions based on the account_id.
+        """
+
+        pass
+    
+    def goflat_order_request(self):
+        """
+        Liquidate  all positions and cancel all unresolved order requests 
+        based on the account_id.
+        """
+
+        pass
+    
+    def send(self, 
+             request_type: RequestType,
+             request_details: dict):
+        """
+        Master function for running LiveOrder object. Send order requests to 
+        the exchange provided some request details matching the request types.
+
+        Parameters
+        ----------
+        request_type : str
+            DESCRIPTION.
+        request_details : dict
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        match request_type:
+            case RequestType.NEW_ORDER:
+                # For new_order_request -> return OrderID
+                self.new_order_request(**request_details)
+    
+            case RequestType.MODIFY_ORDER:
+                # For other oder_requests, use the OrderID from new_order_request
+                self.modify_order_request(**request_details)
+            
+            case RequestType.CANCEL_ORDER:
+                self.cancel_order_request(**request_details)
+            
+            case RequestType.ACRIVATE_ORDER:
+                self.activate_order_request(**request_details)
+            
+            case RequestType.CANCELALL_ORDER:
+                self.cancelall_order_request(**request_details)
+                
+            case RequestType.LIQUIDATEALL_ORDER:
+                self.liquidateall_order_request(**request_details)
+                
+            case RequestType.GOFLAT_ORDER:
+                self.goflat_order_request(**request_details)
