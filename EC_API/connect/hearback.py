a2e893ddbd73ab3ef5d59a0808be17260c9cb582
Dexter <dex-hon-sci@outlook.com>
2025-08-14 16:49:29 +0800

    making hearback proper decorator

--- EC_API/connect/hearback.py
+++ EC_API/connect/hearback.py
@@ -6,41 +6,48 @@ Created on Thu Jul 31 13:25:41 2025
 @author: dexter
 """
 import time
+from typing import Callable
 from google.protobuf import json_format
 from EC_API.ext.WebAPI.webapi_2_pb2 import ClientMsg, ServerMsg
 from EC_API.msg_validation.CQG_valid_msg_check import CQGValidMsgCheck
 
-def hearback(client,  
-             client_msg: ClientMsg = None,
-             hearback_time: float = 30) -> ServerMsg | None:
-    # A decorator function in charge of hearing back a message 
-    # immediately after sending it
-    start_time = time.time()
-    
-    while True:
-        server_msg = client.receive_server_message()
-        # Msg Check 
-        check = CQGValidMsgCheck(client_msg, server_msg)
-        check.check_all()
-        return_msg =  check.return_msg() 
-        if return_msg is not None:
-            return return_msg
         
-        duration = time.time() - start_time
-        if duration > hearback_time:
-            break
+def hearback(func: Callable[str, int], 
+             hearback_time: float =30, *args, **kwargs): 
+    client, client_msg = func(*args, **kwargs) 
+    def wrapper() -> ServerMsg:  
+        start_time = time.time()
         
-def get_contract_metadata(client, client_msg: ClientMsg):
-    # Check if client_msg is an information_request
-    
-    while True:
-        server_msg = client.receive_server_message()
-        check = CQGValidMsgCheck(client_msg, server_msg)
-        check.check_all()
-        return_msg =  check.return_msg() 
-        if return_msg is not None:
-            contract_metadata = server_msg.information_reports[0].symbol_resolution_report.contract_metadata
-    return contract_metadata
+        while True:
+            server_msg = client.receive_server_message()
+            # Msg Check 
+            check = CQGValidMsgCheck(client_msg, server_msg)
+            check.check_all()
+            return_msg =  check.return_msg() 
+            if return_msg is not None:
+                return return_msg
+            
+            duration = time.time() - start_time
+            if duration > hearback_time:
+                return 
+    return wrapper
+
+def get_contract_metadata(func: Callable[str, int], 
+                          *args, **kwargs):
+    client, client_msg = func(*args, **kwargs)
+    def wrapper() -> ServerMsg:
+        # Check if client_msg is an information_request
+
+        while True: # hearback loop
+            server_msg = client.receive_server_message()
+            check = CQGValidMsgCheck(client_msg, server_msg)
+            check.check_all()
+            return_msg =  check.return_msg() 
+            if return_msg is not None:
+                contract_metadata = server_msg.information_reports[0].symbol_resolution_report.contract_metadata
+        return contract_metadata
+    return wrapper
+        
 
 def render_json(server_msg: ServerMsg) -> str:
     json_string = json_format.MessageToJson(server_msg, indent=2)
