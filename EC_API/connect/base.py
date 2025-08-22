40d3e3a4ec1c76b98a0a2864cee72a60b9df4dd6
Dexter <dex-hon-sci@outlook.com>
2025-08-22 12:50:12 +0800

    Adding new enums and instructions in the connect protocol class

--- EC_API/connect/base.py
+++ EC_API/connect/base.py
@@ -7,6 +7,7 @@ Created on Wed Jul 30 10:23:04 2025
 """
 # Native Python imports
 from typing import Protocol
+from EC_API.connect.enums import ConnectionState
 
 class Connect(Protocol):
     # Base class for websocket-like connection
@@ -14,22 +15,66 @@ class Connect(Protocol):
                  host_name: str, 
                  user_name: str, 
                  password: str):
+        """
+        Instantiation can immediately create connection to the server.
+
+        """
         self._host_name = host_name
         self._user_name = user_name
-        self._password = password
-        
-        # Immediate connection
+        self._password = password       
+        self._state = ConnectionState.UNKNOWN
     
     @property
     def client(self):
-        # return client connection object
+        """
+        Getter method for client connection object.
+        """
         return self._client
+    
+    def connect(self):
+        """
+        Create connection to the server.
+        State: UKNOWN -> CONNECTING/
+        if it is successful, State: CONNECTING -> CONNECTED
+        if it is not, State: CONNECTING -> DISCONNECTED
+        """
+        pass
 
     def logon(self):
+        """
+        User initiated Logon sequence. 
+        if it is successful, State: CONNECTED -> CONNECTED_LOGON
+        if it is not, State: CONNECTED -> CONNECTED
+
+        """
         pass
     
     def logoff(self):
+        """
+        User initiated Logoff sequence.
+        if it is successful, State: CONNECTED_LOGON -> CONNECTED_LOGOFF
+        if it is not, State: CONNECTED_LOGON -> CONNECTED_LOGON
+
+        """
+        pass
+    
+    def restore_request(self):
+        """
+        A Necessary restore connection method. Usually it take in a 
+        session_token as an input to restore connection.
+        State: DISCONNECTED -> RECONNECTING
+        if it is successful, State: RECONNECTING -> CONNECTED
+        if it is not, State: RECONNECTING -> DISCONNECTED
+
+        """
         pass
     
     def disconnect(self):
+        """
+        User initiated Disconnection sequence.
+        State: CONNECTED/CONNECTED_LOGON/CONNECTED_LOGOFF -> DISCONNECTING
+        if it is successful, State: DISCONNECTING -> DISCONNECTED
+        if it is not, State: DISCONNECTING -> CONNECTED/CONNECTED_LOGON/CONNECTED_LOGOFF
+
+        """
         pass
--- EC_API/connect/enums.py
+++ EC_API/connect/enums.py
@@ -9,11 +9,14 @@ Created on Fri Aug 22 10:04:11 2025
 from enum import Enum
 
 class ConnectionState(Enum):
-    CONNECTED = "Connected" # Handshake is completed
-    CONNECTING = "Connecting" # Attempting to Establish communication
+    CONNECTED = "Connected" # Connection is Established without either logon or logoff
+    CONNECTED_LOGON = "Connected-Logon" # Connection is Established and Logon is success
+    CONNECTED_LOGOFF = "Connected-Logoff" # Connection is Established and Lofoff is success
+    CONNECTING = "Connecting" # Attempting to Establish communication (handshake not confirm)
     DISCONNECTED = "Disconnected" # Connection is lost
     RECONNECTING = "Reconnecting" # Reconnection based on Fault tolerance policy
     CLOSING = "Closing" # User initiated Disconnection
     CLOSED = "Closed" # State once the underlying connection is closed.    
+    #Aborted = "Aborted"
     UNKNOWN = "Unknown" # Default Setting, to be changed once connection is established
     
