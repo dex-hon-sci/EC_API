d00c3aed9c387fbdad288fc84754d1ce0096225a
Dexter <dex-hon-sci@outlook.com>
2025-09-22 13:17:06 +0800

    Preserve Time-based buffer for inactive period, allow for two versions for dev preference

--- EC_API/monitor/tick.py
+++ EC_API/monitor/tick.py
@@ -5,13 +5,15 @@ Created on Mon Sep  8 09:32:34 2025
 
 @author: dexter
 """
-from collections import deque
+# Python imports
+from typing import Protocol
 from dataclasses import dataclass
+from collections import deque
 import time
+# Python Package imports
 import numpy as np
-
+# EC_API imports
 from EC_API.utility.base import time_it
-
 
 @dataclass
 class Tick:
@@ -19,7 +21,13 @@ class Tick:
     volume: int
     timestamp: int  # Unix timestamp
 
-class TickBuffer:
+class TickBuffer(Protocol):
+    def __init__(self): pass
+    def add_tick(self): pass
+    def current_tick(self): pass
+    
+    
+class RingTickBuffer(TickBuffer):
     def __init__(self, size:int):
         self.size = size
         self.prices = np.zeros(size, dtype=np.float64)
@@ -43,13 +51,17 @@ class TickBuffer:
     def get_window(self, 
                    horizon: float | None = None,
                    current_time: float | None = None) -> tuple[np.array[int|float]]:
+        """
+        - If horizon is None → return last N ticks (tick-based window).
+        - If horizon is given → filter ticks by time (time-based window).
+        """
         if self.full:
             prices, vols, ts = self.prices, self.volumes, self.timestamps
         else:
             prices, vols, ts = self.prices[:self.cur], self.volumes[:self.cur], self.timestamps[:self.cur]
         
         if horizon is not None and current_time is not None:
-            mask = ts >=current_time - horizon
+            mask = ts >=current_time - horizon #filter condition in timestamp array
             return prices[mask], vols[mask], ts[mask]
         
     def __len__(self):
@@ -66,73 +78,61 @@ class TickBuffer:
             timestamp=float(self.timestamps[last_index]),
         )
     
-# =============================================================================
-# @dataclass
-# class Tick:
-#     price: int
-#     volume: int
-#     timestamp: int  # Unix timestamp
-#     
-# class TickBuffer:
-#     def __init__(self, timeframes_seconds: int | float):
-#         """
-#         timeframes_seconds: single float or list of floats for 
-#         multi-timeframe buffers
-#         """
-#         if isinstance(timeframes_seconds, (int, float)):
-#             timeframes_seconds = [timeframes_seconds]
-#         self.timeframes = timeframes_seconds
-#         self.buffers = {tf: deque(np.array([])) for tf in self.timeframes}
-#         
-#         #self.price_buffers = {tf: deque() for tf in self.timeframes}
-#         #self.volume_buffers = {tf: deque() for tf in self.timeframes}
-#         #self.timestamp_buffers = {tf: deque() for tf in self.timeframes}
-#         
-#     @time_it
-#     def add_tick(self, 
-#                  price: int, 
-#                  volume: int, 
-#                  timestamp: int = None):
-#         if timestamp is None:
-#             timestamp = time.time()
-#         tick = Tick(price, volume, timestamp)
-#         for tf, buf in self.buffers.items():
-#             buf.append(tick)
-#             self._pop_old_ticks(buf, tf, timestamp)
-#     @time_it
-#     def _pop_old_ticks(self, buf, 
-#                        timeframe: float, 
-#                        current_time: float):
-#         """Remove ticks older than timeframe"""
-#         while buf and buf[0].timestamp < current_time - timeframe:
-#             buf.popleft()
-#             
-#     def _select_buffer(self, tf):
-#         if tf is None:
-#             tf = self.timeframes[0]
-#         return self.buffers[tf]
-#             
-#     def ohlc(self, tf=None):
-#         buf = self._select_buffer(tf)
-#         if not buf:
-#             return None
-#         prices = [t.price for t in buf]
-#         return {
-#             "Open": prices[0],
-#             "High": max(prices),
-#             "Low": min(prices),
-#             "Close": prices[-1]
-#             }
-# 
-#     def size(self, tf=None):
-#         buf = self._select_buffer(tf)
-#         return len(buf)
-# 
-#     def clear(self, tf=None):
-#         if tf is None:
-#             for buf in self.buffers.values():
-#                 buf.clear()
-#         else:
-#             self.buffers[tf].clear()
-# =============================================================================
+class TimeTickBuffer(TickBuffer):
+    def __init__(self, timeframes_seconds: int | float):
+        """
+        timeframes_seconds: single float or list of floats for 
+        multi-timeframe buffers
+        """
+        if isinstance(timeframes_seconds, (int, float)):
+            timeframes_seconds = [timeframes_seconds]
+        self.timeframes = timeframes_seconds
+        self.buffers = {tf: deque(np.array([])) for tf in self.timeframes}
+
+    @time_it
+    def add_tick(self, 
+                 price: int, 
+                 volume: int, 
+                 timestamp: int = None) -> None:
+        if timestamp is None:
+            timestamp = time.time()
+        tick = Tick(price, volume, timestamp)
+        for tf, buf in self.buffers.items():
+            buf.append(tick)
+            self._pop_old_ticks(buf, tf, timestamp)
+    @time_it
+    def _pop_old_ticks(self, buf, 
+                       timeframe: float, 
+                       current_time: float) -> None:
+        """Remove ticks older than timeframe"""
+        while buf and buf[0].timestamp < current_time - timeframe:
+            buf.popleft()
+            
+    def _select_buffer(self, tf) -> deque[np.array[float|int]]:
+        if tf is None:
+            tf = self.timeframes[0]
+        return self.buffers[tf]
+            
+    def ohlc(self, tf=None) -> dict[str, float|int]:
+        buf = self._select_buffer(tf)
+        if not buf:
+            return None
+        prices = [t.price for t in buf]
+        return {
+            "Open": prices[0],
+            "High": max(prices),
+            "Low": min(prices),
+            "Close": prices[-1]
+            }
+
+    def size(self, tf=None) -> int:
+        buf = self._select_buffer(tf)
+        return len(buf)
+
+    def clear(self, tf=None) -> None:
+        if tf is None:
+            for buf in self.buffers.values():
+                buf.clear()
+        else:
+            self.buffers[tf].clear()
             
