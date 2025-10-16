a031a03736b570bf619b85361d8cf31420ff7f10
Dexter <dex-hon-sci@outlook.com>
2025-10-16 20:25:57 +0800

    Adding example DB for further testing

--- tests/example_actiontree.py
+++ tests/example_actiontree.py
@@ -5,6 +5,8 @@ Created on Tue Sep 30 19:22:46 2025
 
 @author: dexter
 """
+import uuid
+import asyncio
 from datetime import datetime, timedelta, timezone
 from EC_API.op_strategy.action import ActionNode, ActionTree
 from EC_API.op_strategy.signal import OpSignal
@@ -31,7 +33,7 @@ overtime_cond = lambda ctx: ctx.feeds['Asset_A'].tick_buffer.buffers[timeframe][
 # Define Payloads for asset A
 TE_PL_A = Payload(  
     account_id=ACCOUNT_ID,
-    request_id=101,
+    request_id=100,
     status = PayloadStatus.PENDING,
     order_request_type = RequestType.NEW_ORDER,
     start_time = datetime.now(timezone.utc) +\
@@ -143,9 +145,12 @@ overtime_PL_A = Payload(
     check_method = checks
     )
 # Define Database for ActionNode
-DB_SESSION = None
-TO_DB_TABLE = None
-SCAN_DB_TABLES = None
+from tests.example_db import TEST_ASYNC_SESSION, TestStorage, init_db
+DB_SESSION = TEST_ASYNC_SESSION
+TO_DB_TABLE = TestStorage
+SCAN_DB_TABLES = TestStorage
+# Initialise test DB
+asyncio.run(init_db())
 
 # Define Action Nodes
 cancel_node = ActionNode("CancelEntry", 
@@ -158,17 +163,28 @@ cancel_node = ActionNode("CancelEntry",
 TP_node_1 = ActionNode("TakeProfit1", 
                        payloads=[TP_PL1_A], 
                        trigger_cond = TP_trigger_1, 
-                       transitions={})
+                       transitions={},
+                       db_session = DB_SESSION,
+                       to_db_table = TO_DB_TABLE,
+                       scan_db_tables = SCAN_DB_TABLES)
+
 TP_node_2 = ActionNode("TakeProfit2", 
                        payloads =[TP_PL2_A], 
                        trigger_cond = TP_trigger_2, 
-                       transitions={})
+                       transitions={},
+                       db_session = DB_SESSION,
+                       to_db_table = TO_DB_TABLE,
+                       scan_db_tables = SCAN_DB_TABLES)
+
 TE_node_mod = ActionNode("ModifyTargetEntry", 
                          payloads=[TE_mod_PL_A], 
                          trigger_cond = mod_TE_trigger, 
                          transitions={
                              "TakeProfit2": (TP_trigger_2, TP_node_2)
-                             })
+                             },
+                         db_session = DB_SESSION,
+                         to_db_table = TO_DB_TABLE,
+                         scan_db_tables = SCAN_DB_TABLES)
 TE_node = ActionNode("TargetEntry", 
                      payloads=[TE_PL_A], # Have two assets for testing. Same direction
                      trigger_cond = TE_trigger, 
@@ -176,11 +192,17 @@ TE_node = ActionNode("TargetEntry",
                          "TakeProfit1": (TP_trigger_1, TP_node_1),
                          "ModifyTargetEntry": (mod_TE_trigger, TE_node_mod),
                          "CancelEntry": (cancel_trigger, cancel_node)   
-                         })
+                         },
+                       db_session = DB_SESSION,
+                       to_db_table = TO_DB_TABLE,
+                       scan_db_tables = SCAN_DB_TABLES)
 overtime_node = ActionNode("OvertimeExit", 
                            payloads=[overtime_PL_A], 
                            trigger_cond=overtime_cond, 
-                           transitions={})
+                           transitions={},
+                           db_session = DB_SESSION,
+                           to_db_table = TO_DB_TABLE,
+                           scan_db_tables = SCAN_DB_TABLES)
 
 # Define Action Tree
 tree = ActionTree(TE_node, overtime_cond, overtime_node)
---
+++ tests/example_db.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 16 15:46:18 2025
+
+@author: dexter
+"""
+from pathlib import Path
+
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import Column, Integer, Enum as SqlEnum
+from sqlalchemy import DateTime
+from sqlalchemy.types import PickleType
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+# EC Trade System import
+from EC_API.payload.enums import PayloadStatus
+from EC_API.ordering.enums import RequestType
+
+
+#BASE_DIR = Path(__file__).resolve().parent
+BASE_DIR = "/home/dexter/Euler_Capital_codes/EC_API/tests/test_orders.db"
+DATABASE_URL = f"sqlite+aiosqlite:///{BASE_DIR}"
+engine = create_async_engine(DATABASE_URL, echo=False)
+Base = declarative_base()
+
+TEST_ASYNC_SESSION = async_sessionmaker(engine, expire_on_commit=False)
+
+print("DATABASE_URL", DATABASE_URL)
+class BaseTable(Base):
+    __abstract__ = True  
+
+    request_id = Column(Integer, primary_key=True)
+    account_id = Column(Integer)
+    status = Column(SqlEnum(PayloadStatus), nullable=False)
+    order_request_type = Column(SqlEnum(RequestType), nullable=False)
+    start_time = Column(DateTime, nullable=False) # Effective time
+    end_time = Column(DateTime, nullable=False) # Expiration time
+    order_info = Column(PickleType, nullable=True)
+    
+class TestStorage(BaseTable):
+    __tablename__ = "Test_Storage"
+    
+    def __repr__(self):
+        return f"<Test_Storage(id={self.request_id}, status={self.status}, type={self.order_request_type})>"
+
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
