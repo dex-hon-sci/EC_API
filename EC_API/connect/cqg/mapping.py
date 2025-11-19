6eb399082c2dc72febcda873da2873ef217b4989
Dexter <dex-hon-sci@outlook.com>
2025-11-19 17:52:26 +0800

    Adding the mapping module for cqg to map between connection state and server logon status

---
+++ EC_API/connect/cqg/mapping.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 19 15:54:46 2025
+
+@author: dexter
+"""
+
+from EC_API.connect.enums import ConnectionState
+from EC_API.ext.WebAPI.user_session_2_pb2 import LogonResult as LOR
+from EC_API.ext.WebAPI.user_session_2_pb2 import LoggedOff as LOff
+from EC_API.ext.WebAPI.user_session_2_pb2 import RestoreOrJoinSessionResult as Restore
+
+CONNMSG2CONNSTATE_MAP = {
+    # Failed Logon attempts but connection still intact
+    LOR.ResultCode.RESULT_CODE_FAILURE: ConnectionState.CONNECTED_DEFAULT,
+    LOR.ResultCode.RESULT_CODE_NO_ONETIME_PASSWORD: ConnectionState.CONNECTED_DEFAULT,
+    LOR.ResultCode.RESULT_CODE_PASSWORD_EXPIRED:ConnectionState.CONNECTED_DEFAULT,
+    LOR.ResultCode.RESULT_CODE_CONCURRENT_SESSION:ConnectionState.CONNECTED_DEFAULT,
+    LOR.ResultCode.RESULT_CODE_REDIRECTED: ConnectionState.CONNECTED_DEFAULT,
+    LOR.ResultCode.RESULT_CODE_ROUTINE_ERROR: ConnectionState.CONNECTED_DEFAULT,
+    LOR.ResultCode.RESULT_CODE_ACCESS_TOKEN_EXPIRED: ConnectionState.CONNECTED_DEFAULT,
+    Restore.ResultCode.RESULT_CODE_FAILURE: ConnectionState.CONNECTED_DEFAULT,
+    Restore.ResultCode.RESULT_CODE_UNKNOWN_SESSION: ConnectionState.CONNECTED_DEFAULT,
+    Restore.ResultCode.RESULT_CODE_ACCESS_DENIED: ConnectionState.CONNECTED_DEFAULT,
+    Restore.ResultCode.RESULT_CODE_INVALID_PARAMS: ConnectionState.CONNECTED_DEFAULT,
+    # Successful Logon/Logoff enum
+    LOR.ResultCode.RESULT_CODE_SUCCESS: ConnectionState.CONNECTED_LOGON,
+    Restore.ResultCode.RESULT_CODE_SUCCESS: ConnectionState.CONNECTED_LOGON,
+    LOff.LogoffReason.LOGOFF_REASON_BY_REQUEST: ConnectionState.CONNECTED_LOGOFF,
+    LOff.LogoffReason.LOGOFF_REASON_REDIRECTED: ConnectionState.CONNECTED_LOGOFF,
+    LOff.LogoffReason.LOGOFF_REASON_FORCED: ConnectionState.CONNECTED_LOGOFF,
+    LOff.LogoffReason.LOGOFF_REASON_REASSIGNED: ConnectionState.CONNECTED_LOGOFF,    
+    }
--- EC_API/connect/enums.py
+++ EC_API/connect/enums.py
@@ -9,13 +9,15 @@ Created on Fri Aug 22 10:04:11 2025
 from enum import Enum
 
 class ConnectionState(Enum):
-    CONNECTED = "Connected" # Connection is Established without either logon or logoff
+    # Account Login related enums
+    CONNECTED_DEFAULT = "Connected-Default" # Connection is Established without either logon or logoff
     CONNECTED_LOGON = "Connected-Logon" # Connection is Established and Logon is success
     CONNECTED_LOGOFF = "Connected-Logoff" # Connection is Established and Lofoff is success
+    # Connection related enums
     CONNECTING = "Connecting" # Attempting to Establish communication (handshake not confirm)
-    DISCONNECTED = "Disconnected" # Connection is lost
+    DISCONNECTED = "Disconnected" # Connection is lost (Ping indicate problem)
     RECONNECTING = "Reconnecting" # Reconnection based on Fault tolerance policy
-    CLOSING = "Closing" # User initiated Disconnection
+    CLOSING = "Closing" # User initiated Disconnection (Disconnect command)
     CLOSED = "Closed" # State once the underlying connection is closed.    
     #Aborted = "Aborted"
     UNKNOWN = "Unknown" # Default Setting, to be changed once connection is established
