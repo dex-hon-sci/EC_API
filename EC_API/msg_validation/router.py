db0c66669a526ca4996bc16988c23c8eff80b53c
Dexter <dex-hon-sci@outlook.com>
2025-11-27 10:40:56 +0800

    Adding router script to msg_validation

---
+++ EC_API/msg_validation/router.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 26 16:40:57 2025
+
+@author: dexter
+"""
+
+class MessageRouter:
+    def __init__(self):
+        self._pending = {}
+        
+    def register(self, key):
+        pass
