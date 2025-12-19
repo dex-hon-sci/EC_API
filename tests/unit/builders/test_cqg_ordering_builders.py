1f0a9fe63539f00e05cce5fcd889b62086a7baf5
Dexter <dex-hon-sci@outlook.com>
2025-12-20 07:59:26 +0800

    Building up tests for ordering builders

--- tests/unit/builders/test_cqg_ordering_builders.py
+++ tests/unit/builders/test_cqg_ordering_builders.py
@@ -5,14 +5,15 @@ Created on Thu Dec 18 18:43:55 2025
 
 @author: dexter
 """
+from datetime import datetime, timezone, timedelta
+
 from EC_API.ordering.cqg.enums import Duration
 from EC_API.ordering.cqg.builders import (
-    build_modify_order_request_msg_2
+    build_modify_order_request_msg
     )
 
 ACCOUNT_ID = 000000
 REQUEST_ID = 100
-from datetime import datetime, timezone
 
 def test_build_trade_subscription_msg_valid() -> None:
     pass
@@ -23,10 +24,12 @@ def test_build_new_order_request_msg_valid() -> None:
 def test_build_modify_order_request_msg_valid() -> None:
     
     mod_datetime = datetime.now(timezone.utc)
+    goodthru_datetime = mod_datetime + timedelta(hours=1)
+    activation_datetime = mod_datetime + timedelta(hours=2)
     
-    msg = build_modify_order_request_msg_2(
+    msg = build_modify_order_request_msg(
         ACCOUNT_ID, REQUEST_ID,
-        order_id = 200, 
+        order_id = "200", 
         orig_cl_order_id = "300",
         cl_order_id = "400",
         when_utc_timestamp = mod_datetime,
@@ -37,8 +40,8 @@ def test_build_modify_order_request_msg_valid() -> None:
         remove_suspension_utc_time = True,
         duration = Duration.DURATION_ATC, 
         good_thru_date = 10001,
-        good_thru_utc_timestamp = 10002, 
-        activation_utc_timestamp = 10003,
+        good_thru_utc_timestamp = goodthru_datetime, 
+        activation_utc_timestamp = activation_datetime,
         extra_attributes = None,
         )
     assert msg.modify_order.account_id == ACCOUNT_ID
