a704d79c6cdc4be9435c15ae75d1f8ecc4b84190
Dexter <dex-hon-sci@outlook.com>
2025-12-19 23:22:36 +0800

    Finished connect builder tests, working on ordering builder tests

--- tests/unit/builders/test_cqg_connect_builders.py
+++ tests/unit/builders/test_cqg_connect_builders.py
@@ -6,15 +6,21 @@ Created on Thu Dec 18 18:41:33 2025
 @author: dexter
 """
 
-from EC_API.ext.WebAPI.webapi_2_pb2 import ClientMsg
-from EC_API.ext.WebAPI.user_session_2_pb2 import Logon, Logoff
+from EC_API.ext.WebAPI.webapi_2_pb2 import (
+    ClientMsg, InformationRequest,
+    )
+from EC_API.ext.WebAPI.user_session_2_pb2 import ( 
+    Logon, Logoff, 
+    RestoreOrJoinSession,
+    Ping
+    )
 from EC_API.connect.cqg.builders import (
     build_logon_msg, build_logoff_msg,
     build_restore_msg, build_ping_msg,
     build_pong_msg, build_resolve_symbol_msg
     )
 
-def test_build_logon_msg() -> None:
+def test_build_logon_msg_valid() -> None:
     msg = build_logon_msg(
         'user_name', 'password',
         client_app_id = 'WebApiTest-unit-test', 
@@ -36,18 +42,44 @@ def test_build_logon_msg() -> None:
     assert msg.logon.drop_concurrent_session == False
     assert msg.logon.private_label == 'private-label-unit-test'
 
-def test_build_logoff_msg() -> None:
+def test_build_logoff_msg_valid() -> None:
     msg = build_logoff_msg(
         txt_msg="Test_logoff"
         )
     assert type(msg.logoff) == Logoff
-    assert msg.logoff.txt_msg == "Test_logoff"
+    assert msg.logoff.text_message == "Test_logoff"
     
-def test_build_restore_msg() -> None:
-    pass
+def test_build_restore_msg_valid() -> None:
+    msg = build_restore_msg(
+        client_app_id ='WebApiTest-unit-test', 
+        protocol_version_major = 303, 
+        protocol_version_minor = 240,
+        session_token = "ABCDEFG",
+        )
+    
+    assert type(msg.restore_or_join_session) == RestoreOrJoinSession
+    restor_msg = msg.restore_or_join_session
+    assert restor_msg.client_app_id =='WebApiTest-unit-test'
+    assert restor_msg.protocol_version_major == 303
+    assert restor_msg.protocol_version_minor == 240
+    assert restor_msg.session_token == "ABCDEFG"
 
-def test_build_ping_msg() -> None:
-    pass
+def test_build_ping_msg_valid() -> None:
+    msg = build_ping_msg(ping_utc_time=10)
+    
+    assert type(msg.ping) == Ping
+    assert msg.ping.ping_utc_time == 10
 
-def test_build_resolve_symbol_msg() -> None:
-    pass
+def test_build_resolve_symbol_msg_valid() -> None:
+    msg = build_resolve_symbol_msg(symbol_name="CLEV25",
+                                   request_id= 101,
+                                   subscribe=True,
+                                   instrument_group_request="CLE")
+    
+    assert type(msg.information_requests[0]) == InformationRequest
+    assert msg.information_requests[0].symbol_resolution_request.symbol == "CLEV25"    
+    assert msg.information_requests[0].id == 101
+    assert msg.information_requests[0].subscribe == True
+    instrument_gp_request = msg.information_requests[0].instrument_group_request
+    assert instrument_gp_request.instrument_group_id == "CLE"
+    
--- tests/unit/builders/test_cqg_ordering_builders.py
+++ tests/unit/builders/test_cqg_ordering_builders.py
@@ -5,46 +5,65 @@ Created on Thu Dec 18 18:43:55 2025
 
 @author: dexter
 """
+from EC_API.ordering.cqg.enums import Duration
 from EC_API.ordering.cqg.builders import (
     build_modify_order_request_msg_2
     )
 
 ACCOUNT_ID = 000000
 REQUEST_ID = 100
+from datetime import datetime, timezone
 
-def test_build_trade_subscription_msg() -> None:
+def test_build_trade_subscription_msg_valid() -> None:
     pass
 
-def test_build_new_order_request_msg() -> None:
+def test_build_new_order_request_msg_valid() -> None:
     pass
 
-def test_build_modify_order_request_msg() -> None:
+def test_build_modify_order_request_msg_valid() -> None:
+    
+    mod_datetime = datetime.now(timezone.utc)
+    
     msg = build_modify_order_request_msg_2(
         ACCOUNT_ID, REQUEST_ID,
         order_id = 200, 
         orig_cl_order_id = "300",
-        cl_order_id = "400"
+        cl_order_id = "400",
+        when_utc_timestamp = mod_datetime,
+        qty = 27, 
+        scaled_limit_price = 12300,
+        scaled_stop_price = 12400,
+        remove_activation_time = True,
+        remove_suspension_utc_time = True,
+        duration = Duration.DURATION_ATC, 
+        good_thru_date = 10001,
+        good_thru_utc_timestamp = 10002, 
+        activation_utc_timestamp = 10003,
+        extra_attributes = None,
         )
     assert msg.modify_order.account_id == ACCOUNT_ID
     assert msg.modify_order.request_id == REQUEST_ID
     assert msg.modify_order.order_id == 200
     assert msg.modify_order.orig_cl_order_id == "300"
     assert msg.modify_order.cl_order_id == "400"
+    assert msg.modify_order.qty == 27
+    
+    assert msg.modify_order.algo_strategy == "CQG Builder UNIT TEST"
 
-def test_build_cancel_order_request_msg() -> None:
+def test_build_cancel_order_request_msg_valid() -> None:
     pass
 
-def test_build_cancelall_order_request_msg() -> None:
+def test_build_cancelall_order_request_msg_valid() -> None:
     pass
 
-def test_build_suspend_order_request_msg() -> None:
+def test_build_suspend_order_request_msg_valid() -> None:
     pass
 
-def test_build_activate_order_request_msg() -> None:
+def test_build_activate_order_request_msg_valid() -> None:
     pass
 
-def test_build_liquidateall_order_request_msg() -> None:
+def test_build_liquidateall_order_request_msg_valid() -> None:
     pass
 
-def test_build_goflat_order_request_msg() -> None:
+def test_build_goflat_order_request_msg_valid() -> None:
     pass
