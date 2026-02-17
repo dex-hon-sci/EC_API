377a8e17ebd6ce0a76d3f0b9eb576e4c4baf09a5
Dexter <dex-hon-sci@outlook.com>
2026-02-18 06:51:07 +0800

    Commence building dummy server for unit test

---
+++ tests/unit/fixtures/dummy_server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 18 06:34:47 2026
+
+@author: dexter
+"""
+
+class DummyServer:
+    
+    def __init__(self):
+        ...
+    
+    
+    # Build up messages
+    # Unleash
