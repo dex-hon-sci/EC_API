10a2407fdca54cfda71043acee236828e467ca30
Dexter <dex-hon-sci@outlook.com>
2026-03-01 08:22:21 +0800

    Testes valid behaviour for StreamRouter sub and unsub function, refactoring publish function

--- EC_API/transport/routers.py
+++ EC_API/transport/routers.py
@@ -6,35 +6,34 @@ Created on Wed Nov 26 16:40:57 2025
 @author: dexter
 """
 import asyncio
-from typing import Any #Hashable, Optional
+from typing import Any  # Hashable, Optional
 import logging
 
 logger = logging.getLogger(__name__)
 
-#(msg_family, msg_type, id_field_name, id)
-RouterKey = tuple[str, str, str, int|str] 
- 
+# (msg_family, msg_type, id_field_name, id)
+RouterKey = tuple[str, str, str, int | str]
+
+
 class MessageRouter:
     def __init__(self):
         self._pending: dict[RouterKey, asyncio.Future] = {}
 
     def register_key(self, key: RouterKey) -> asyncio.Future:
         fut = asyncio.get_running_loop().create_future()
-        # show error if key already exists ...
+
         if key in self._pending.keys():
             logger.error(
-                "router register key failed: key {key} already exist.",
+                "[Message Router] Router register key failed: key '{key}' already exist.",
                 extra={"router_key": key}
-                )
+            )
         self._pending[key] = fut
-        
-        # clearup code in case of user cancel
-        # add_done_callback
 
-        #def _cleanup(_):
-        #    self._pending.pop(key, None)
-        #     
-        #fut.add_done_callback(_cleanup)        
+        # clearup code in case of user cancel
+        def _cleanup(_):
+            self._pending.pop(key, None)
+        
+        fut.add_done_callback(_cleanup)
         return fut
 
     def on_message(self, key: RouterKey, msg: Any) -> bool:
@@ -50,57 +49,84 @@ class MessageRouter:
             if not fut.done():
                 fut.set_exception(exc)
         self._pending.clear()
-        
-        
+
+
 class StreamRouter:
     def __init__(
-            self, 
-            max_queue_size: int = 1_000, 
-            drop_if_full: bool = False
+        self,
+        max_queue_size: int = 1_000,
+        max_sub_size: int = 5,
+        max_num_sym: int = 50,
+        drop_if_full: bool = False
         ):
         # we assume one Stream Router per vendor
         self._subs: dict[int, list[asyncio.Queue]] = {}
         self._max_queue_size = max_queue_size
+        self._max_subs_size = max_sub_size
+        self._max_num_sym = max_num_sym
         self._drop_if_full = drop_if_full
 
     def subscribe(
-            self, 
-            contract_id: int
-        ) -> asyncio.Queue[Any]:
-        q = asyncio.Queue(maxsize=self._max_queue_size) if self._max_queue_size else asyncio.Queue()
+        self,
+        contract_id: int
+        ) -> asyncio.Queue[Any] | None:
         # Add a new Queue to the list in case there is a new subscriber who
         # calls subscribe
+        if len(self._subs) >= self._max_num_sym:
+            logger.error("[Stream Router] Maximum number of contract subscribed exceeded.")
+            return 
+            
+        if self._subs.get(contract_id):
+            if len(self._subs[contract_id]) >= self._max_subs_size:
+                logger.error(f"[Stream Router] Maximum subscribers has reached for this contract: {contract_id}")
+                return
+
+        q = asyncio.Queue(
+            maxsize=self._max_queue_size) if self._max_queue_size else asyncio.Queue()
+
         self._subs.setdefault(contract_id, []).append(q)
         return q
-    
+
+
     def unsubscribe(
-            self, 
-            contract_id: int, 
-            q: asyncio.Queue
+        self,
+        contract_id: int,
+        q: asyncio.Queue
         ) -> None:
-        
+
         lst = self._subs.get(contract_id, [])
         if q in lst:
             lst.remove(q)
+            
+        if q not in lst:
+            logger.error(f"[Stream Router] Unsubscribe failed. Input queue is not in the list of contract id:{contract_id}",
+                         extra = {"contract_id": contract_id}
+                         )
+            return 
+        
         if not lst and contract_id in self._subs:
             # Maybe do some backup for unconsumed data first here
             # Only delete when the streaming is completely done
-            
             del self._subs[contract_id]
-            
+
     async def publish(self, contract_id: int, item: Any) -> None:
+        queues = self._subs.get(contract_id)
+        if not queues:
+            return
+        
         # Put the latest data into the Async Queue inside the StreamRouter
         for q in self._subs.get(contract_id, []):
-            if self._drop_if_full:
-                # Drop newest (or implement drop-oldest policy)
-                continue
+            #if self._drop_if_full:
+            #    # Drop oldest 
             try:
-                await q.put_nowait(item)
+                q.put_nowait(item)
             except asyncio.QueueFull:
-                
-                if not self._drop_if_full:
-                    # safest fallback: drop anyway rather than deadlock
-                    pass
+                if self._drop_if_full:
+                # safest fallback: drop anyway rather than deadlock
+                    try:
+                        q._queue.popleft()
+                        q.put_nowait(item)
+                    except Exception:
+                        continue
 
-        
-
+                    
--- tests/unit/router/test_stream_routers.py
+++ tests/unit/router/test_stream_routers.py
@@ -7,15 +7,51 @@ Created on Sat Feb 28 01:33:13 2026
 """
 import asyncio
 import pytest
-from EC_AportPI.transport.router import StreamRouter
+from EC_API.transport.routers import StreamRouter
 
-
-def test_subscribe():...
+@pytest.mark.asyncio
+async def test_subscribe_valid() -> None:
+    SR = StreamRouter()
+    SR.subscribe(1)
+    SR.subscribe(1)
+    SR.subscribe(1)
+    print(SR._subs)
+    assert len(SR._subs) == 1
+    assert len(SR._subs[1]) == 3
     
-def test_unsubscribe():...
-
-def test_publish():...
+    for i in range(3):
+        assert isinstance(SR._subs[1][i], asyncio.Queue)
+        
+@pytest.mark.asyncio
+async def test_unsubscribe_valid() -> None: 
+    SR = StreamRouter()
+    q1_1 = SR.subscribe(1)
+    q1_2 = SR.subscribe(1)
+    q2 = SR.subscribe(2)
     
-    # subscribe
-    # unsubscribe
-    # publish
+    SR.unsubscribe(1, q1_1)
+    assert len(SR._subs[1]) == 1
+    assert len(SR._subs[2]) == 1
+    assert isinstance(SR._subs[1][0], asyncio.Queue)
+    assert isinstance(SR._subs[2][0], asyncio.Queue)
+
+@pytest.mark.asyncio
+async def test_unsubscribe_invalid_q() -> None:
+    SR = StreamRouter()
+    q1_1 = SR.subscribe(1)
+    q1_2 = SR.subscribe(1)
+    q2 = SR.subscribe(2)
+    
+    SR.unsubscribe(1, q2)
+    assert len(SR._subs[1]) == 2
+    
+@pytest.mark.asyncio
+async def test_publish_valid() -> None:
+    ...
+
+@pytest.mark.asyncio
+async def test_drop_if_full_valid() -> None:
+     SR = StreamRouter(max_queue_size=5)
+     
+
+    
