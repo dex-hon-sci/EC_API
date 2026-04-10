1e39817735ac32e49545a37dc19aa9ef779ac745
Dexter <dex-hon-sci@outlook.com>
2026-04-11 00:18:46 +0800

    Monitor builder+parser unit tests added

--- tests/unit/builders/test_monitor_builders_CQG.py
+++ tests/unit/builders/test_monitor_builders_CQG.py
@@ -5,7 +5,7 @@ Created on Thu Dec 18 18:43:36 2025
 
 @author: dexter
 """
-
+import pytest
 from datetime import datetime, timezone, timedelta
 from EC_API.monitor.cqg.builders import (
     build_realtime_data_request_msg,
@@ -14,6 +14,7 @@ from EC_API.monitor.cqg.builders import (
 from EC_API.monitor.enums import MktDataSubLevel
 from EC_API.monitor.cqg.enums import MktDataSubLevelCQG
 from EC_API.ext.WebAPI.market_data_2_pb2 import MarketDataSubscription as CQG_MDS
+from EC_API.exceptions import MsgBuilderError
 
 def test_build_realtime_data_request_msg_valid() -> None:
     msg = build_realtime_data_request_msg(
@@ -28,16 +29,29 @@ def test_build_realtime_data_request_msg_valid() -> None:
 
 def test_build_reset_tracker_request_msg_valid() -> None:
     msg = build_reset_tracker_request_msg(
-        contract_id= 102,
+        contract_id = 102,
         request_id = 222,
-
         )
     assert msg.market_data_subscriptions[0].contract_id == 102
     assert msg.market_data_subscriptions[0].request_id == 222
     assert msg.market_data_subscriptions[0].level == CQG_MDS.Level.LEVEL_NONE
 
 
-
+def test_build_realtime_data_request_msg_invalid_inputs() -> None:
+    with pytest.raises(MsgBuilderError):
+        build_realtime_data_request_msg(
+            contract_id= "101", # <--wrong inputs
+            request_id = "111", #
+            level = MktDataSubLevelCQG.LEVEL_END_OF_DAY
+            )
+        
+def test_build_reset_tracker_request_msg_invalid_inputs() -> None:
+    with pytest.raises(MsgBuilderError):
+        build_reset_tracker_request_msg(
+            contract_id = '102',
+            request_id = '222',
+            )
+        
 #_instrument_gp_request
 # test_instrument_gp_request_sub
 # test_sym_underlying
--- tests/unit/parsers/test_monitor_parsers_CQG.py
+++ tests/unit/parsers/test_monitor_parsers_CQG.py
@@ -5,26 +5,52 @@ Created on Mon Mar 23 00:35:02 2026
 
 @author: dexter
 """
+from EC_API.ext.WebAPI.webapi_2_pb2 import ServerMsg
+from EC_API.ext.WebAPI.market_data_2_pb2 import MarketDataSubscriptionStatus as MktDSubStatus
+
+from EC_API.monitor.enums import MktDataSubLevel
+from EC_API.monitor.cqg.enum_mapping import MKTDATASUBLEVEL_MAP_INT2CQG
 from EC_API.monitor.cqg.parsers import (
     parse_real_time_market_data,
+    parse_market_data_subscription_statuses
     )
+
 from tests.unit.fixtures.server_msg_streams_CQG import (
+    build_market_data_subscription_statuses_server_msg,
     build_real_time_market_data_server_msg,
     dummy_realtime_data_stream, 
     dummy_order_update_stream
     )
-from EC_API._typing import MarketValueType
+from EC_API._typing import MarketValueType, QuotesValueType
 
-def test_parser_real_data_stream_valid() -> None:
-    server_msg = build_real_time_market_data_server_msg()
-    res = parse_real_time_market_data(server_msg)
-    
-    print(res)
+
+def test_parse_market_data_subscription_statuses_valid() -> None:
+    server_msg = build_market_data_subscription_statuses_server_msg(ServerMsg(),contract_id=3)
+    res = parse_market_data_subscription_statuses(server_msg)
     
     assert isinstance(res, list)
-    assert len(res) == 2
+    assert len(res) == 1
     for ele in res:
-        assert isinstance(ele,tuple)
+        assert isinstance(ele, dict)
+        assert ele['contract_id'] == 3
+        assert ele['status_code'] == MktDSubStatus.StatusCode.STATUS_CODE_SUCCESS
+        assert ele['level'] == MKTDATASUBLEVEL_MAP_INT2CQG[MktDataSubLevel.LEVEL_TRADES]
+   
+def test_parse_market_data_subscription_statuses_invalid() -> None:...
+
+# =============================================================================
+# def test_parser_real_data_stream_valid() -> None:
+#     server_msg = build_real_time_market_data_server_msg()
+#     res = parse_real_time_market_data(server_msg)
+#     
+#     print(res)
+#     
+#     assert isinstance(res, list)
+#     assert len(res) == 2
+#     for ele in res:
+#         assert isinstance(ele,tuple)
+#     
+# =============================================================================
     
     
     
