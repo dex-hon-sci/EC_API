f14e40b82478cc164276242f124ce3bf0485e8c5
Dexter <dex-hon-sci@outlook.com>
2026-04-15 18:25:02 +0800

    Finalise ordering module parsers function and their unittest

--- EC_API/ordering/cqg/parsers.py
+++ EC_API/ordering/cqg/parsers.py
@@ -8,6 +8,7 @@ Created on Sat Apr 11 03:05:48 2026
 from typing import Any
 from google.protobuf.json_format import MessageToDict
 from EC_API.ext.WebAPI.webapi_2_pb2 import ServerMsg
+from EC_API.ext.WebAPI.trade_routing_2_pb2 import PositionStatus
 from EC_API.protocol.cqg.parser_util import register_parser
 from EC_API.exceptions import MsgParserError
 
@@ -63,11 +64,9 @@ def parse_trade_subscription_statuses(
 def parse_trade_snapshot_completions(
         msg:ServerMsg
     ) -> list[dict[str, Any]]:
-
     try:
         res = []
         for ele in msg.trade_snapshot_completions:
-            print('ele', ele)
             res.append({
                 "sub_id": ele.subscription_id,
                 "sub_scopes": list(ele.subscription_scopes)
@@ -78,11 +77,13 @@ def parse_trade_snapshot_completions(
 
 @register_parser('order_statuses')
 def parse_order_statuses(
-        msg:ServerMsg
+        msg: ServerMsg,
+        in_detail: bool = False
     ) -> list[dict[str, Any]]:
     try:
         res = []
         for i, ele in enumerate(msg.order_statuses):
+            # parsing required fields
             D = {
                 "sub_ids": list(ele.subscription_ids),
                 "order_id": ele.order_id,
@@ -92,8 +93,11 @@ def parse_order_statuses(
                 "fill_cnt": ele.fill_cnt,
                 "scaled_avg_fill_price": ele.scaled_avg_fill_price,
                 "avg_fill_price_correct": ele.avg_fill_price_correct,
-                "account_id": ele.account_id
+                "account_id": ele.account_id,
+                'entered_by_user': ele.entered_by_user
                 }
+            
+            # missiion critical optional fields parsing
             if ele.HasField('order'):
                 D['order'] = {
                     'account_id': ele.order.account_id,
@@ -105,6 +109,13 @@ def parse_order_statuses(
                     'qty': {'significand': ele.order.qty.significand,
                             'exponent':ele.order.qty.exponent}
                     }
+                
+            # Parse all the detailed optional field in the message. Slow Method.
+            if in_detail: 
+                # Note: int32/float/double stays numeric, anything int64/uint64
+                # becomes a string.  A flaw in JSON formatting. Fix later.
+                detail = MessageToDict(ele, preserving_proto_field_name=True)
+                D.update({k: v for k, v in detail.items() if k not in D})
             res.append(D)
         return res
     
@@ -113,18 +124,49 @@ def parse_order_statuses(
 
 @register_parser('position_statuses')
 def parse_position_statuses(
-        msg:ServerMsg
+        msg: ServerMsg,
+        in_detail: bool = False
     ) -> list[dict[str, Any]]:
 
     try:
         res = []
         for i, ele in enumerate(msg.position_statuses):
-            ...
+            D = {
+                'sub_ids': list(ele.subscription_ids),
+                'account_id': ele.account_id,
+                'contract_id': ele.contract_id,
+                'is_short_open_position': ele.is_short_open_position,
+                
+                }
+            if ele.open_positions:
+                poss = parse_open_position(ele)
+                D['open_positions'] = poss
+                
+            # Parse all the detailed field in the message. Slow.
+            if in_detail: 
+                # Note: int32/float/double stays numeric, anything int64/uint64
+                # becomes a string. A flaw in JSON formatting. Fix later.
+                detail = MessageToDict(ele, preserving_proto_field_name=True)
+                D.update({k: v for k, v in detail.items() if k not in D})
+                        
+            res.append(D)
         return res
     
     except Exception as e:
         raise MsgParserError(f"Failed to parse position_statuses: {e}") from e
-
+    
+def parse_open_position(msg: PositionStatus): # special helper for fast execution
+    poss = []
+    for pos in msg.open_positions:
+        poss.append({
+            'id': pos.id,
+            'price_correct': pos.price_correct,
+            'trade_date': pos.trade_date,
+            'statement_date': pos.statement_date,
+            'is_aggregated': pos.is_aggregated,
+            'is_short': pos.is_short
+            })
+    return poss
     
 @register_parser('account_summary_statuses')
 def parse_account_summary_statuses(
--- tests/unit/parsers/test_ordering_parsers_CQG.py
+++ tests/unit/parsers/test_ordering_parsers_CQG.py
@@ -77,7 +77,7 @@ def test_parse_order_statuses_server_msg_valid() -> None:
         ServerMsg(),
         order = client_msg.order_requests[0].new_order.order)
     res = parse_order_statuses(msg)
-
+    
     assert isinstance(res, list)
     assert len(res) == 1
     assert res[0]['sub_ids'] == [1]
@@ -87,6 +87,7 @@ def test_parse_order_statuses_server_msg_valid() -> None:
     assert res[0]['scaled_avg_fill_price'] == -200
     assert res[0]['avg_fill_price_correct'] == 10.0
     assert res[0]['account_id'] == 123466
+    assert res[0]['entered_by_user'] == "user_A"
     
     assert res[0]['order']['account_id'] == 123466
     assert res[0]['order']['contract_id'] == 10
@@ -97,8 +98,127 @@ def test_parse_order_statuses_server_msg_valid() -> None:
     
 def test_parse_position_statuses_server_msg_valid() -> None:
     msg = build_position_statuses_server_msg(ServerMsg())
-    parse_position_statuses(msg)
-
+    res = parse_position_statuses(msg)
+    assert isinstance(res, list)
+    assert len(res) == 1
+    assert res[0]['sub_ids'] == [0,1,2]
+    assert res[0]['account_id'] == 123466
+    assert res[0]['contract_id'] == 0
+    assert res[0]['is_short_open_position'] == False
+    
+    assert res[0]['open_positions'][0]['id'] == 2
+    assert res[0]['open_positions'][0]['price_correct'] == 101
+    assert res[0]['open_positions'][0]['is_aggregated'] == True
+    assert res[0]['open_positions'][0]['is_short'] == False
+    assert 'trade_date' in res[0]['open_positions'][0].keys()
+    assert 'statement_date' in res[0]['open_positions'][0].keys()
+    
+# =============================================================================
+# def build_position_statuses_server_msg(
+#         server_msg: ServerMsg,
+#         subscription_ids: list[int] = [0,1,2],
+#         contract_id: int = 0
+#     ) -> ServerMsg:    
+#     
+#     position_statuses = server_msg.position_statuses.add()
+#     for num in subscription_ids:
+#         position_statuses.subscription_ids.append(num)
+#     
+#     position_statuses.account_id = 123466
+#     position_statuses.contract_id = contract_id
+#     position_statuses.is_short_open_position = False
+#     
+#     # ----
+#     open_positions = position_statuses.open_positions.add()
+#     open_positions.id = 2
+#     open_positions.price_correct = 101
+#     open_positions.trade_date = int(datetime.now().timestamp())
+#     open_positions.statement_date = int(datetime.now().timestamp())
+#     open_positions.is_aggregated = True
+#     open_positions.is_short = False
+#     
+#     # ----
+#     purchase_and_sales_groups = position_statuses.purchase_and_sales_groups.add()
+#     purchase_and_sales_groups.id = 4
+#     purchase_and_sales_groups.realized_profit_loss = 10
+#     
+#     matched_trades = purchase_and_sales_groups.matched_trades.add()
+#     matched_trades.price = 292
+#     matched_trades.trade_date = int(datetime.now().timestamp())
+#     matched_trades.statement_date = int(datetime.now().timestamp())
+#     matched_trades.is_aggregated = True
+#     # ----
+#     today_fill_commissions = position_statuses.today_fill_commissions.add()
+#     today_fill_commissions.commission_currency = "USD"
+#     today_fill_commissions.commission = 102
+#     return server_msg
+# =============================================================================
+# =============================================================================
+# def build_order_statuses_server_msg(
+#         server_msg: ServerMsg,
+#         res: OrderStatus.Status = OrderStatus.Status.IN_TRANSIT,
+#         contract_id: int = 0,
+#         sub_id: int = 1,
+#         order_id: str = "order_id_1",
+#         chain_order_id: str = "chain_order_id_1",
+#         order = None
+#     ) -> ServerMsg:
+#     
+#     order_statuses = server_msg.order_statuses.add()
+#     
+#     # ----
+#     order_statuses.subscription_ids.append(sub_id)
+#     order_statuses.status = res
+#     order_statuses.order_id = order_id
+#     order_statuses.chain_order_id = chain_order_id
+#     order_statuses.status_utc_timestamp = datetime.now()
+#     order_statuses.submission_utc_timestamp = datetime.now()
+#     order_statuses.fill_cnt = 0
+#     order_statuses.scaled_avg_fill_price = -200
+#     order_statuses.avg_fill_price_correct = 10
+#     #----
+#     transaction_statuses = order_statuses.transaction_statuses.add()
+#     transaction_statuses.status = TransactionStatus.Status.IN_TRANSIT
+#     transaction_statuses.trans_id = 2
+#     transaction_statuses.trans_utc_timestamp = datetime.now()
+#     transaction_statuses.cl_order_id = "cl_order_id_1"
+#     
+#     trades = transaction_statuses.trades.add()
+#     trades.trade_id = "trade_id_1"
+#     trades.contract_id = 0
+#     trades.statement_date = int(datetime.now().timestamp())
+#     trades.trade_utc_timestamp = datetime.now()
+#     
+#     trades.scaled_price = 1000 #price = round(price_correct / correct_price_scale)
+#     trades.price_correct = 100100
+#     trades.side = Ord.Side.SIDE_BUY
+#     # ----
+#     order_statuses.entered_by_user = "user_A"
+#     order_statuses.first_statement_date = int(datetime.now().timestamp())
+#    
+#     if order is not None: 
+#         order_statuses.order.CopyFrom(order)
+#     # ----
+#     contract_metadata = order_statuses.contract_metadata.add()
+#     contract_metadata.contract_id = 0
+#     contract_metadata.contract_symbol = "Symbol_1"
+#     contract_metadata.correct_price_scale = 0.01
+#     contract_metadata.display_price_scale = 202
+#     contract_metadata.description = "description"
+#     contract_metadata.title = "title"
+#     contract_metadata.tick_size = 10
+#     contract_metadata.currency = "USD"
+#     contract_metadata.tick_value = 0
+#     contract_metadata.cfi_code = "ESVUFB"
+#     contract_metadata.instrument_group_name = "instru_group_1"
+#     contract_metadata.session_info_id  = 232323
+#     contract_metadata.short_instrument_group_name = "short_instrument_group_name"
+#     contract_metadata.instrument_group_description = "instrument_group_description"
+#     contract_metadata.country_code = "US"
+#     # ----
+#     order_statuses.account_id = 123466    
+#     return server_msg
+# =============================================================================
 def test_parse_account_summary_statuses_server_msg_valid() -> None:
     msg = build_account_summary_statuses_server_msg(ServerMsg())
     res = parse_account_summary_statuses(msg)
@@ -115,7 +235,48 @@ def test_parse_go_flat_statuses_server_msg_valid() -> None:
     assert res[0]['request_id'] == 1
     assert res[0]['account_id'] == 1210221
     assert res[0]['status_code'] == GFltStatus.StatusCode.STATUS_CODE_COMPLETED
+    
+# --- Detail parsing
+def test_parse_order_statuses_server_msg_in_detail_valid() -> None:
+    msg = build_order_statuses_server_msg(
+        ServerMsg(),
+        )
+    res = parse_order_statuses(msg, in_detail=True)
+    print(res)
+    assert res[0].get('transaction_statuses') is not None
+    assert res[0].get('contract_metadata') is not None
+    assert res[0]['transaction_statuses'][0]['trans_id'] == '2'
+    assert res[0]['transaction_statuses'][0]['cl_order_id'] == "cl_order_id_1"
+    assert res[0]['transaction_statuses'][0]['trades'][0]['trade_id'] == "trade_id_1"
+    assert res[0]['transaction_statuses'][0]['trades'][0]['contract_id'] == 0
+    assert res[0]['transaction_statuses'][0]['trades'][0]['scaled_price'] == '1000'
+    assert res[0]['transaction_statuses'][0]['trades'][0]['price_correct'] == 100100
+    assert res[0]['transaction_statuses'][0]['trades'][0]['side'] == 1
 
+    assert res[0]['contract_metadata'][0]['contract_id'] == 0
+    assert res[0]['contract_metadata'][0]['contract_symbol'] == "Symbol_1"
+    assert res[0]['contract_metadata'][0]['correct_price_scale'] == 0.01
+    assert res[0]['contract_metadata'][0]['display_price_scale'] == 202
+    assert res[0]['contract_metadata'][0]['description'] == "description"
+    assert res[0]['contract_metadata'][0]['title'] == "title"
+    assert res[0]['contract_metadata'][0]['tick_size'] == 10
+    assert res[0]['contract_metadata'][0]['currency'] == "USD"
+    assert res[0]['contract_metadata'][0]['tick_value'] == 0
+    assert res[0]['contract_metadata'][0]['cfi_code'] == "ESVUFB"
+    assert res[0]['contract_metadata'][0]['instrument_group_name'] == "instru_group_1"
+    assert res[0]['contract_metadata'][0]['session_info_id'] == 232323
+    assert res[0]['contract_metadata'][0]['short_instrument_group_name'] == "short_instrument_group_name"
+    assert res[0]['contract_metadata'][0]['instrument_group_description'] == "instrument_group_description"
+    assert res[0]['contract_metadata'][0]['country_code'] == "US"
+
+    
+def test_parse_position_statuses_server_msg_in_detail_valid() -> None: 
+    msg = build_position_statuses_server_msg(ServerMsg())
+    res = parse_position_statuses(msg, in_detail = True)
+    assert res[0].get('purchase_and_sales_groups') is not None
+    assert res[0]['purchase_and_sales_groups'][0].get('matched_trades') is not None
+    assert res[0].get('today_fill_commissions') is not None
+    
 
 # --- Sad Path
 def test_parse_order_request_rejects_null() -> None:
