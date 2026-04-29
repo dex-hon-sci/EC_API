eacd976941f6f9724cd104f00c4681e1aa1bbc68
Dexter <dex-hon-sci@outlook.com>
2026-04-29 19:48:37 +0800

    tests script planned for object life cycles

---
+++ tests/unit/connect/test_connect_object_lifecycle_CQG.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 18:05:32 2026
+
+@author: dexter
+"""
+
---
+++ tests/unit/monitor/test_monitor_data_object_lifecycle_CQG.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 29 19:20:21 2026
+
+@author: dexter
+"""
+
