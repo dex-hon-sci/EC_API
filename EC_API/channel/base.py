5161963ce11011f52cbdea02efc061f17b3d6021
Dexter <dex-hon-sci@outlook.com>
2026-05-13 23:03:18 +0800

    Adding a new module 'channel' to handle IPC related tasks. Drafted a protocol class for further development. Implementeation for Redis, socket, pipes come next.

---
+++ EC_API/channel/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 13 19:44:46 2026
+
+@author: dexter
+"""
+from typing import Protocol, Callable, Any
+
+class Channel(Protocol):
+    on_data: Callable[[Any], None]    # wired to DataBus.push at engine construction
+
+    async def connect(self) -> None: ...
+    async def disconnect(self) -> None: ...
+
+    async def broadcast(self) -> None: ...
+
+    async def listen(self) -> None: ...
