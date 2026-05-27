c636d869f152e8a5a32291ec77e113ccebf1af9d
Dexter <dex-hon-sci@outlook.com>
2026-05-27 16:38:13 +0800

    add tick_buffers in cpp format.

--- EC_API/common/tick_buffers_ext.cpp
+++ EC_API/common/tick_buffers_ext.cpp
@@ -1,19 +1,36 @@
+#include <Python.h>
 #include <deque>
+#include <string>
 #include <pybind11/pybind11.h>
 
 
 /*Extraction -> tick -> buffers*/
 
-void extract_raw_tick() {};
+void extract_raw_tick(PyObject* obj, std::string instruction) {
+// Pick the right list first, then loop through it 
+// only read the right indices, copy them to a Tick struct 
+    if !PyList_Check(obj) {
+        throw std::invalid_argument("Expected a list")
+    }
+};
 
 struct TradeTick {double timestamp, price, volume};
+struct Tick {double timestamp, price, volume};
 
 
 class SlidingWindowBuffer {
+    double _window; // time window
+    std::deque<Tick> _ticks;
+    double _sum_price = 0, sum_price = 0; 
+    double _sum_pricevol = 0, _sum_vol = 0;
     int _n = 0;
+    
 public:
-    void add_raw_tick() {};
-    void add_tick() {};
+    SldingWindowBuffer() {}
+    void add_tick() {
+        extract_raw_tick
+    
+    };
 
 };
 
