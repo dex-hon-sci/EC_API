992c8a0eefddf8c4d6c4d51c154c8fe173a80a77
Dexter <dex-hon-sci@outlook.com>
2026-05-31 02:48:22 +0800

    cleanup imports

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ from pybind11.setup_helpers import Pybind11Extension, build_ext
 ext_modules = [
     Pybind11Extension(
         "EC_API.common.tick_buffers_ext",
-        sources=["EC_API/common/tick_buffers_ext.cpp"],
-        include_dirs=["EC_API"],                 
+        sources=[
+            "EC_API/common/tick_buffers_ext.cpp",
+            "EC_API/common/data_extractors_cqg.cpp"
+            ],
+        include_dirs=["EC_API/common"],                 
     ),
 ]
 
